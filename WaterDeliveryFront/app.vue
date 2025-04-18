28026f5c8e40184e19702f334552b1c6f19d7e33
Everleaff <gr2001gr@mail.ru>
2025-04-19 00:13:46 +0900

    Добавлен Nuxt и Tailwind

---
+++ WaterDeliveryFront/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    
+  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+</template>
+
+<script>
+
+
+</script>
---
+++ WaterDeliveryFront/nuxt.config.ts
@@ -0,0 +1,41 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    'nuxtjs-naive-ui'
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ],
+  },
+})
